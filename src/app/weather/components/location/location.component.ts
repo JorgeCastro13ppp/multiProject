c6762051fd6c48baa25766c0ac3b81e9b26a4d23
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-05-03 14:32:24 +0200

    falta

--- src/app/weather/components/location/location.component.ts
+++ src/app/weather/components/location/location.component.ts
@@ -11,33 +11,22 @@ import { Dia } from '../../interfaces/dias.interface';
 export class LocationComponent implements OnInit {
 
   daysOfWeek = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
-  apiData?:ApiCall;
+  // apiData?:ApiCall;
   dayData?:Dia;
   constructor(private locationService:LocationServiceService) { }
 
   ngOnInit(): void {
     this.locationService.fetchApi().subscribe(
-      (api:ApiCall)=>{
-        this.apiData = api;
-        console.log('Weather data',this.apiData);
-        console.log('Url data: ',this.apiData.datos)
+      (day:Dia)=>{
+        this.dayData = day;
+        console.log('Day data:', this.dayData );
       },
       (error)=>{
-        console.error('Error fetching',error);
+        console.error('Error fetching API data',error);
       }
     );
   }
 
-  getWeatherDay():void {
-    this.locationService.fetchData().subscribe(
-      (day:Dia)=>{
-        this.dayData=day;
-        console.log('Day data',this.dayData);
-      },
-      (error)=>{
-        console.error('Error fetching',error);
-      }
-    );
-  }
+
 
 }
