6689db0e1e4cf262463f9e4da3ca7e30215407c7
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-05-30 17:06:14 +0200

    arreglar navbar, home-page, no alerts, interceptor, calculadora, responsive aemet, cursor pointer paises , altura cal 100vh , input map

--- src/app/core/components/weather-map/weather-map.component.ts
+++ src/app/core/components/weather-map/weather-map.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-// import * as L from 'leaflet';
+//  import * as L from 'leaflet';
 
 @Component({
   selector: 'app-weather-map',
